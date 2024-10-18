632113a37de5f3914f7a02d4fc9601545622a554
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2024-10-17 21:01:40 -0600

    Se agrega descrpición del ejercicio

--- 4.1.2_Modulos/CommonJS/index.js
+++ 4.1.2_Modulos/CommonJS/index.js
@@ -16,3 +16,11 @@ console.log(constantes.gravedades.tierra);
 // Accediendo con destructuración
 console.log(sumar(10, 10));
 console.log(gravedades.tierra);
+
+// Ejercicio: 
+/*
+Busca al menos dos de los programas de JavaScript que hayas realizado anteriormente, adaptalos y ejecutalos usando Node js.
+Se deben cumplir las siguientes condiciones:
+    - Los programas deben requerir de entradas de usuario (leer datos desde la terminal con Node)
+    - Utilizar al menos un import y export.
+*/
