637ce045a8b42d3a442700a5bae7c8b71ecaff0f
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2024-10-22 21:13:36 -0600

    Se agrega descripción del ejercicio

--- 4.2.1_Callbacks/setTimeout.js
+++ 4.2.1_Callbacks/setTimeout.js
@@ -17,3 +17,29 @@ while(!ejecucionTerminada){
     console.log('Esperando temporizador...');
 }
 */
+
+/*
+
+Ejercicio de callbacks
+1. Crear 3 funciones diferentes para simular un inicio de sesión, estas funciones reciben 
+como parámetros un usuario y contraseña.
+2. Crear una función que reciba un callback y reciba el usuario y contraseña.
+3. La función que recibe el callback debe ejecutarlo y regresar verdadero si 
+los valores recibidos permiten iniciar sesión o no.
+
+*/
+
+function iniciarSesionAlumno(usuario, password){
+    const alumnos = ['Pedro11', 'María32'];
+    const passwords = ['12345', 'contraseña'];
+
+    let sesionValida;
+
+    // Verificar que los parámetros recibidos existan;
+
+    return sesionValida;
+}
+
+function iniciarSesion(callback, usuario, password){
+    
+}
