85aee3d73a774985b947d43ae0c61eb0df5656d1
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2024-10-24 19:32:26 -0600

    Se agrega ejemplo de promesas anidadas

--- 4.2.2_Promesas/index.js
+++ 4.2.2_Promesas/index.js
@@ -1,8 +1,8 @@
 let temporizador1 = new Promise((resolve, reject) => {
     setTimeout(() => {
         let numeroAleatorio = Math.floor(Math.random() * 10);
-        console.log(`Valor generado: ${numeroAleatorio}`);
-        if(numeroAleatorio > 5){
+        console.log(`Valor generado en temporizador 1: ${numeroAleatorio}`);
+        if(numeroAleatorio > 0){
             resolve('El valor es mayor a 5. :)');
         }else{
             reject('El valor es menor que 5. :(');
@@ -11,11 +11,38 @@ let temporizador1 = new Promise((resolve, reject) => {
 });
 
 
+
 temporizador1.then((valorPromesaCumplida) => {
     console.log('Promesa cumplida');
     console.log(valorPromesaCumplida);
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let numeroAleatorio = Math.floor(Math.random() * 10);
+            console.log(`Valor generado en temporizador 2: ${numeroAleatorio}`);
+            if(numeroAleatorio > 0){
+                resolve('El valor es mayor a 5. :)');
+            }else{
+                reject('El valor es menor que 5. :(');
+            }
+        }, 5000);
+    });
+}).then((valorPromesaCumplida) => {
+    console.log('Promesa cumplida');
+    console.log(valorPromesaCumplida);
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let numeroAleatorio = Math.floor(Math.random() * 10);
+            console.log(`Valor generado en temporizador 3: ${numeroAleatorio}`);
+            if(numeroAleatorio > 5){
+                resolve('El valor es mayor a 5. :)');
+            }else{
+                reject('El valor es menor que 5. :(');
+            }
+        }, 1000);
+    });
 }).catch((valorPromesaRechazada) => {
     console.log('Promesa rechazada');
     console.log(valorPromesaRechazada);
 });
-
