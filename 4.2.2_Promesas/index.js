d2fa7be52875ef502a555d7ec36ad68a84f92863
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2024-10-24 21:03:42 -0600

    Se agrega descripción del ejercicio

--- 4.2.2_Promesas/index.js
+++ 4.2.2_Promesas/index.js
@@ -142,3 +142,18 @@ Promise.race([temporizador1, temporizador2, temporizador3])
     console.log('Falló la primera promesa');
     console.log(valorPromesaRechazada);
 });
+
+// Ejercicio
+/* Vamos a cocinar un pastel y para eso debemos seguir los siguientes pasos:
+    1. Comprar los ingredientes -> 5 segundos
+    2. Preparar los ingredientes -> 2 segundos
+    3. Revolver los ingredientes -> 3 segundos
+    4. Precalentar el horno -> 4 segundos
+    5. Hornear mezcla -> 5 segundos
+    6. Servir pastel -> 1 segundos
+    Tiempo total = 20 segundos.
+
+    1. Simular cada paso con setTimeout y console logs y van preparar su pastel.
+
+    2. Optimizar el proceso. Utilizar procesos asíncronos para ahorrar tiempo en la preparación del pastel.
+*/
