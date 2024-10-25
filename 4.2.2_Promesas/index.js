df45e3d761d7d66664734235a15ec4bb2754c056
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2024-10-24 19:57:00 -0600

    Se agrega ejemplo de Promise.all() y Promise.allSettled()

--- 4.2.2_Promesas/index.js
+++ 4.2.2_Promesas/index.js
@@ -1,3 +1,4 @@
+/*
 let temporizador1 = new Promise((resolve, reject) => {
     setTimeout(() => {
         let numeroAleatorio = Math.floor(Math.random() * 10);
@@ -45,4 +46,71 @@ temporizador1.then((valorPromesaCumplida) => {
 }).catch((valorPromesaRechazada) => {
     console.log('Promesa rechazada');
     console.log(valorPromesaRechazada);
+});
+
+*/
+
+let temporizador1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let numeroAleatorio = Math.floor(Math.random() * 10);
+        console.log(`Valor generado en temporizador 1: ${numeroAleatorio}`);
+        if(numeroAleatorio > 3){
+            resolve('El valor es mayor a 3. :)');
+        }else{
+            reject('El valor es menor que 3. :(');
+        }
+    }, 2000);
+});
+
+let temporizador2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let numeroAleatorio = Math.floor(Math.random() * 10);
+        console.log(`Valor generado en temporizador 2: ${numeroAleatorio}`);
+        if(numeroAleatorio > 3){
+            resolve('El valor es mayor a 3. :)');
+        }else{
+            reject('El valor es menor que 3. :(');
+        }
+    }, 5000);
+});
+
+let temporizador3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let numeroAleatorio = Math.floor(Math.random() * 10);
+        console.log(`Valor generado en temporizador 3: ${numeroAleatorio}`);
+        if(numeroAleatorio > 3){
+            resolve('El valor es mayor a 3. :)');
+        }else{
+            reject('El valor es menor que 3. :(');
+        }
+    }, 1000);
+});
+
+// Promise.all() regresa una promesa resuelta si TODAS las promesas se cumplen.
+// Si CUALQUIERA de las promesas es rechazada, la promesa resultante también es rechazada.
+Promise.all([temporizador1, temporizador2, temporizador3])
+.then((valorPromesaResuelta) => {
+    console.log(`Todas las promesas fueron resuletas. :D`);
+    console.log(valorPromesaResuelta);
+})
+.catch((valorPromesaRechazada) => {
+    console.log(`------------------------------------------`)
+    console.log(`Promise.all()`);
+    console.log(`Una de las promesas fue rechazada. :(`);
+    console.log(valorPromesaRechazada);
+    console.log(`------------------------------------------`)
+
+});
+
+// Promise.allSettled() regresa una promesa resuelta cuando TODAS las promesas terminan (sin importar
+// si se cumplen o se rechazan)
+Promise.allSettled([temporizador1, temporizador2, temporizador3])
+.then((valoresPromesasTerminadas) => {
+    console.log(`------------------------------------------`);
+    console.log(`Promise.allSettled()`);
+    console.log(`Todas las promesas terminaron`);
+    console.log(valoresPromesasTerminadas);
+    console.log(`------------------------------------------`);
+}).catch((valorPromesaFallida) => {
+    console.log(valorPromesaFallida);
 });
